faf3893b12c584d45a2e7751bb8cb057abeeadd5
AlexSerg01 <alex.box.work@icloud.com>
2023-12-22 13:52:53 +0100

    finish.

--- src/js/our-yachts.js
+++ src/js/our-yachts.js
@@ -1,47 +1,41 @@
 export function rotateYachtsList() {
-  var yachtsList = document.querySelector('.yachts-list');
-  var prevButton = document.querySelector('.rotate-left');
-  var nextButton = document.querySelector('.rotate-right');
-
-  var currentIndex = 0; // Поточний індекс видимого елемента
-
-  var intervalId = setInterval(function () {
-    rotateList(yachtsList, 1); // Передача 1 для руху вліво
-  }, 10000);
-
-  prevButton.addEventListener('click', function () {
-    rotateList(yachtsList, -1); // Передача -1 для руху вправо
-    clearInterval(intervalId);
-  });
-
-  nextButton.addEventListener('click', function () {
-    rotateList(yachtsList, 1); // Передача 1 для руху вліво
-    clearInterval(intervalId);
-  });
-
-  function rotateList(list, direction) {
-    var items = list.children;
-    var firstItem = items[0];
-    var lastItem = items[items.length - 1];
-
-    // Визначення напрямку руху
-    if (direction === 1) {
-      currentIndex = (currentIndex + 1) % items.length;
-    } else {
-      currentIndex = (currentIndex - 1 + items.length) % items.length;
-    }
-
-    list.style.transition = 'transform 0.5s ease-in-out';
-    list.style.transform =
-      'translateX(-' + currentIndex * firstItem.offsetWidth + 'px)';
-
-    // Оновлення видимого елемента
-    list.insertBefore(lastItem, firstItem);
-
-    // Зняття анімації після завершення перегортання
-    setTimeout(function () {
-      list.style.transition = 'none';
-      list.style.transform = 'translateX(0)';
-    }, 500);
+  // Функція обертання
+  function rotate() {
+    var yachtsList = document.querySelector('.yachts-list');
+    var firstItem = yachtsList.firstElementChild;
+    yachtsList.appendChild(firstItem);
   }
+
+  // Функція, яка визначає, чи слід виконувати обертання
+  function shouldRotate() {
+    return window.innerWidth <= 1279;
+  }
+
+  // Зберігаємо початковий порядок елементів
+  var initialOrder = Array.from(document.querySelectorAll('.yachts-list > li'));
+
+  // Запускаємо обертання при завантаженні сторінки
+  rotate();
+
+  // Викликати функцію обертання та відновлення початкового порядку при зміні розміру вікна
+  window.addEventListener('resize', function () {
+    var yachtsList = document.querySelector('.yachts-list');
+
+    if (shouldRotate()) {
+      rotate();
+    } else {
+      // Відновлюємо початковий порядок елементів
+      yachtsList.innerHTML = '';
+      initialOrder.forEach(function (item) {
+        yachtsList.appendChild(item);
+      });
+    }
+  });
+
+  // Запускаємо обертання кожні 5 секунд, якщо розмір екрану відповідає умові
+  setInterval(function () {
+    if (shouldRotate()) {
+      rotate();
+    }
+  }, 5000);
 }
